2b135fca48705a0f8596424ddd2905f9e8163900
moiny <findmoin@gmail.com>
2014-10-26 21:54:08 +0530

    first commit

---
+++ src/main/java/com/ymoin/socaltech/harvester/TweetPoller.java
@@ -0,0 +1,11 @@
+package com.ymoin.socaltech.harvester;
+
+/**
+ * Hello world!
+ *
+ */
+public class TweetPoller  {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
